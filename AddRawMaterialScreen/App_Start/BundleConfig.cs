842c44c25a2554d871c721448ab0292d297b2af1
Movarind <u17045780@tuks.co.za>
2019-05-29 10:45:47 +0200

    First commit

---
+++ AddRawMaterialScreen/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AddRawMaterialScreen
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js" , "~/Content/jquery.meanmenu.js", "~/Content/jquery.scrollUp.js", "~/Content/jquery.mCustomScrollbar.concat.min.js" ));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js", "~/Content/main.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"  ));
+
+
+        }
+    }
+}
